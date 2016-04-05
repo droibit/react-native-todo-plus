50b394ad675ee09601482c44385383a7b1ec17c3
Shinya Kumagai <roomful.rooms@gmail.com>
2016-04-06 00:01:10 +0900

    Added actions of todo

--- js/constant/todo-constants.js
+++ js/constant/todo-constants.js
@@ -4,7 +4,9 @@ import {keyMirror} from 'keymirror';
 
 const constants = keyMirror({
   TODO_CREATE: null,
-  TODO_DESTROY: null
+  TODO_DESTROY: null,
+  TODO_COMPLETE: null,
+  TODO_UNDO_COMPLETE: null,
 });
 
 export default constants;
--- js/store/todo-store.js
+++ js/store/todo-store.js
@@ -38,12 +38,37 @@ dispatcher.register(action => {
     case constants.TODO_CREATE:
       let text = action.text.trim();
       if (text !== '') {
-
+        create(text);
+        TodoStore.emitChange();
       }
       break;
     case constants.TODO_DESTROY:
+      destroy(action.id);
+      TodoStore.emitChange();
+      break;
+    case constants.TODO_COMPLETE:
+      update(action.id, {completed: true});
+      TodoStore.emitChange();
       break;
     default:
       throw new Error("Unknown action type: ${action.type}")
   }
 });
+
+function create(text) {
+  const id = Date.now() + Math.round(Math.random() * 1000);
+  _todos[id] = {
+    id: id,
+    completed: false,
+    text: text,
+  }
+}
+
+function update(id, updates) {
+  _todos[id] = Object.assign({}, _todos[id], updates);
+  console.log('Updated: ${_todos[id]}')
+}
+
+function destroy(id) {
+  delete _todos[id];
+}
