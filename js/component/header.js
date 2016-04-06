64291289623467e3fc05031a73a1e4d606122b4e
Shinya Kumagai <roomful.rooms@gmail.com>
2016-04-07 00:36:07 +0900

    Implementedd header

--- js/component/header.js
+++ js/component/header.js
@@ -2,20 +2,44 @@
 
 'use strict';
 
-import {View, Component} from 'react-native';
+import React, {Component, View, TextInput, StyleSheet} from 'react-native';
+import TodoActions from "../action/todo-actions";
 
 export default class Header extends Component {
 
-  getInitialState() {
-    return {
-      value: ''
-    }
+  constructor(props) {
+    super(props)
+    this.state = {value: ''}
   }
 
   render() {
     return (
       <View>
+        <TextInput
+          style={style.header}
+          autoFocus={true}
+          placeholder={'What is the next plan?'}
+          value={this.state.value}
+          onChange={text => this.setState({value: text})}
+          onBlur={() => this._add()}
+        />
       </View>
     );
   }
+
+  _add() {
+    const title = this.state.value;
+    if (title !== '') {
+      TodoActions.create(title)
+      this.setState({value: ''})
+    }
+  }
 }
+
+const style = StyleSheet.create({
+  header: {
+    fontSize: 16,
+    marginLeft: 16,
+    marginRight: 16,
+  }
+})
