219035e4d8f40b60d18f08f05d1428ce82965356
Shinya Kumagai <roomful.rooms@gmail.com>
2016-04-11 19:17:42 +0900

    Added simple style to TodoItem

--- js/component/todo-item.js
+++ js/component/todo-item.js
@@ -3,8 +3,27 @@
  */
 'use strict';
 
-import React, {Component} from 'react-native';
+import React, {StyleSheet, Component, Text} from 'react-native';
 
 export default class TodoItem extends Component {
-  
+
+  render() {
+    return (
+      <View style={style.container}>
+        <Text style={styles.text} />
+      </View>
+    );
+  }
 }
+
+const style = StyleSheet.create({
+  container: {
+    height: 72,
+    paddingLeft: 16,
+    paddingRight: 16,
+  },
+  text: {
+    fontSize: 16,
+    textAlign: 'left',
+  }
+});
